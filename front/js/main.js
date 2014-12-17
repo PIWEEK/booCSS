0f8dbfed1342dc933fcba04d8585dc97836f30aa
Juanfran <juanfran.alcantara@kaleidos.net>
2014-12-17 12:38:27 +0100

    init detail integration

--- front/js/main.js
+++ front/js/main.js
@@ -16,71 +16,98 @@ var App = React.createClass({
                     'status': true,
                     'description': 'blablabla',
                     'date': '11/12/2013',
-                    'image': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
+                    'image': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 2,
+                    'id': 3,
                     'name': 'Maecenas blandit laoreet massa vitae porttitor',
                     'status': true,
                     'description': 'blablabla',
                     'date': '11/12/2005',
-                    'image': 'http://upload.wikimedia.org/wikipedia/commons/c/ca/Ubuntu_GNOME_13.10_ScreenShot.png'
+                    'image': 'http://upload.wikimedia.org/wikipedia/commons/c/ca/Ubuntu_GNOME_13.10_ScreenShot.png',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 2,
+                    'id': 4,
                     'name': 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
                     'status': false,
                     'description': 'blablabla',
                     'date': '11/12/2010',
-                    'image': 'http://i1-news.softpedia-static.com/images/news2/Ubuntu-GNOME-13-10-Alpha-2-Saucy-Salamander-Officially-Released-Screenshot-Tour-371212-6.jpg?1374825407'
+                    'image': 'http://i1-news.softpedia-static.com/images/news2/Ubuntu-GNOME-13-10-Alpha-2-Saucy-Salamander-Officially-Released-Screenshot-Tour-371212-6.jpg?1374825407',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 2,
+                    'id': 5,
                     'name': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     'status': true,
                     'description': 'blablabla',
                     'date': '11/12/2013',
-                    'image': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
+                    'image': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 3,
+                    'id': 6,
                     'name': 'Maecenas blandit laoreet massa vitae porttitor',
                     'status': true,
                     'description': 'blablabla',
                     'date': '11/12/2005',
-                    'image': 'http://upload.wikimedia.org/wikipedia/commons/c/ca/Ubuntu_GNOME_13.10_ScreenShot.png'
+                    'image': 'http://upload.wikimedia.org/wikipedia/commons/c/ca/Ubuntu_GNOME_13.10_ScreenShot.png',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 3,
+                    'id': 7,
                     'name': 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
                     'status': false,
                     'description': 'blablabla',
                     'date': '11/12/2010',
-                    'image': 'http://i1-news.softpedia-static.com/images/news2/Ubuntu-GNOME-13-10-Alpha-2-Saucy-Salamander-Officially-Released-Screenshot-Tour-371212-6.jpg?1374825407'
+                    'image': 'http://i1-news.softpedia-static.com/images/news2/Ubuntu-GNOME-13-10-Alpha-2-Saucy-Salamander-Officially-Released-Screenshot-Tour-371212-6.jpg?1374825407',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 3,
+                    'id': 8,
                     'name': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                     'status': true,
                     'description': 'blablabla',
                     'date': '11/12/2013',
-                    'image': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
+                    'image': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 3,
+                    'id': 9,
                     'name': 'Maecenas blandit laoreet massa vitae porttitor',
                     'status': true,
                     'description': 'blablabla',
                     'date': '11/12/2005',
-                    'image': 'http://upload.wikimedia.org/wikipedia/commons/c/ca/Ubuntu_GNOME_13.10_ScreenShot.png'
+                    'image': 'http://upload.wikimedia.org/wikipedia/commons/c/ca/Ubuntu_GNOME_13.10_ScreenShot.png',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 },
                 {
-                    'id': 3,
+                    'id': 10,
                     'name': 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
                     'status': false,
                     'description': 'blablabla',
                     'date': '11/12/2010',
-                    'image': 'http://i1-news.softpedia-static.com/images/news2/Ubuntu-GNOME-13-10-Alpha-2-Saucy-Salamander-Officially-Released-Screenshot-Tour-371212-6.jpg?1374825407'
+                    'image': 'http://i1-news.softpedia-static.com/images/news2/Ubuntu-GNOME-13-10-Alpha-2-Saucy-Salamander-Officially-Released-Screenshot-Tour-371212-6.jpg?1374825407',
+                    'original': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'new': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg',
+                    'diff': 'http://imagenes.es.sftcdn.net/es/scrn/70000/70565/ubuntu-79-700x525.jpg'
                 }
             ]
         };
@@ -95,7 +122,7 @@ var App = React.createClass({
 var routes = (
   <Route handler={App} path="/">
     <Route name="test" path="/test/:testId" handler={TestDetail}/>
-    <DefaultRoute handler={Tests} />
+    <Route name="main" path="/" handler={Tests}/>
   </Route>
 );
 
--- front/js/views/testDetail.js
+++ front/js/views/testDetail.js
@@ -1,8 +1,72 @@
-var TestDetail = React.createClass({
+var Link = window.ReactRouter.Link;
+
+var TestDetailFail = React.createClass({
     render: function() {
+        var test = this.props.test;
         return (
-            <div className="kk">
-                <p>sdfjdsfj</p>
+            <div className="test-detail test-detail-fail">
+                <div className="inner">
+                    <div className="select-buttons">
+                        <div className="wrap">
+                            <button className="btn btn-raised btn-success">Ok</button>
+                            <button className="btn btn-raised btn-warning">No</button>
+                        </div>
+                    </div>
+                    <Link to="main" title="Return" className="btn btn-fab btn-fab-mini btn-raised btn-primary go-back"><i className="mdi-navigation-arrow-back"></i></Link>
+                    <button title="Delete" className="btn btn-fab btn-fab-mini btn-raised btn-danger delete"><i className="mdi-action-delete"></i></button>
+                    <div className="images">
+                        <div className="original image">
+                            <img src={test.original} />
+                        </div>
+                        <div className="new image">
+                            <img src={test.original} />
+                        </div>
+                        <div className="diff image">
+                            <img src={test.diff} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var TestDetailSuccess = React.createClass({
+    render: function() {
+        var test = this.props.test;
+
+        return (
+            <div className="test-detail test-detail-success">
+                <div className="inner">
+                    <Link to="main" title="Return" className="btn btn-fab btn-fab-mini btn-raised btn-primary go-back"><i className="mdi-navigation-arrow-back"></i></Link>
+                    <button title="Delete" className="btn btn-fab btn-fab-mini btn-raised btn-danger delete"><i className="mdi-action-delete"></i></button>
+                    <div className="image">
+                        <img src={test.original} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var TestDetail = React.createClass({
+    mixins: [window.ReactRouter.State],
+    render: function() {
+        var routeParams = this.getParams();
+        var testId = parseInt(routeParams.testId, 10);
+        var test = _.find(this.props.tests, {id: testId});
+
+        var detail;
+
+        if (test.status) {
+            detail = <TestDetailSuccess test={test} />
+        } else {
+            detail = <TestDetailFail test={test} />
+        }
+
+        return (
+            <div>
+            {detail}
             </div>
         );
   }
