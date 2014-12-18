6de997ec64a82dadaa5d35912c9e5703d683f47a
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2014-12-18 09:23:38 +0100

    Adding lost files

---
+++ back/settings.js
@@ -0,0 +1,26 @@
+require('6to5/polyfill');
+
+// Imports
+import * as fs from 'fs-extra';
+
+function mkdirIfNotExist(path){
+    if (!fs.existsSync(path)){
+        fs.mkdir(path);
+    }
+}
+
+// Constants
+export const TESTS_PATH = `${__dirname}/../../../tests`;
+mkdirIfNotExist(TESTS_PATH);
+export const BASE_TEST_FILE = `${__dirname}/../../../test_base.js`;
+
+export const CASPER_OUTPUT_FOLDER_PATH = `${__dirname}/../../../output`;
+mkdirIfNotExist(CASPER_OUTPUT_FOLDER_PATH);
+export const SCREENSHOTS_OK_FOLDER_PATH = `${__dirname}/../../../screenshots_ok`;
+mkdirIfNotExist(SCREENSHOTS_OK_FOLDER_PATH);
+export const SCREENSHOTS_PENDING_FOLDER_PATH = `${__dirname}/../../../screenshots_pending`;
+mkdirIfNotExist(SCREENSHOTS_PENDING_FOLDER_PATH);
+export const SCREENSHOTS_DIFF_FOLDER_PATH = `${__dirname}/../../../screenshots_diff`;
+mkdirIfNotExist(SCREENSHOTS_DIFF_FOLDER_PATH);
+
+export const DB_PATH = `${__dirname}/../../../db/booCSS.db`;
---
+++ test_base.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var testId = casper.cli.options.testId;
+var outputFile = casper.cli.options.outputFile;
+var screenshotsFolder = casper.cli.options.screenshotsFolder;
+var screenshots = []
+
+casper.start( 'TEST_URL' );
+casper.viewport(1024, 768);
+
+casper.then(function() {
+	capturePath = screenshotsFolder+fs.separator+ testId+'_1.png'
+	this.captureSelector(capturePath, 'body');
+	screenshots.push(capturePath);
+});
+
+casper.run(function(){
+	fs.write(outputFile, JSON.stringify(screenshots), 'w')
+	this.exit();
+});
