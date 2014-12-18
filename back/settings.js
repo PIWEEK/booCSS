adf7a7b01716b101f35f53f174b8a337975eda5e
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2014-12-18 13:44:28 +0100

    Using paths properly

--- back/settings.js
+++ back/settings.js
@@ -2,6 +2,7 @@ require('6to5/polyfill');
 
 // Imports
 import * as fs from 'fs-extra';
+import * as path from 'path';
 
 function mkdirIfNotExist(path){
     if (!fs.existsSync(path)){
@@ -10,17 +11,17 @@ function mkdirIfNotExist(path){
 }
 
 // Constants
-export const TESTS_PATH = `${__dirname}/../../../tests`;
+export const TESTS_PATH = path.normalize(path.join(__dirname,'..','..','..','tests'));
 mkdirIfNotExist(TESTS_PATH);
-export const BASE_TEST_FILE = `${__dirname}/../../../test_base.js`;
+export const BASE_TEST_FILE = path.normalize(path.join(__dirname,'..','..','..','test_base.js'));
 
-export const CASPER_OUTPUT_FOLDER_PATH = `${__dirname}/../../../output`;
+export const CASPER_OUTPUT_FOLDER_PATH = path.normalize(path.join(__dirname,'..','..','..','output'));
 mkdirIfNotExist(CASPER_OUTPUT_FOLDER_PATH);
-export const SCREENSHOTS_OK_FOLDER_PATH = `${__dirname}/../../images/screenshots_ok`;
+export const SCREENSHOTS_OK_FOLDER_PATH = path.normalize(path.join(__dirname,'..','..','images', 'screenshots_ok'));
 mkdirIfNotExist(SCREENSHOTS_OK_FOLDER_PATH);
-export const SCREENSHOTS_PENDING_FOLDER_PATH = `${__dirname}/../../images/screenshots_pending`;
+export const SCREENSHOTS_PENDING_FOLDER_PATH = path.normalize(path.join(__dirname,'..','..','images', 'screenshots_pending'));
 mkdirIfNotExist(SCREENSHOTS_PENDING_FOLDER_PATH);
-export const SCREENSHOTS_DIFF_FOLDER_PATH = `${__dirname}/../../images/screenshots_diff`;
+export const SCREENSHOTS_DIFF_FOLDER_PATH = path.normalize(path.join(__dirname,'..','..','images', 'screenshots_diff'));
 mkdirIfNotExist(SCREENSHOTS_DIFF_FOLDER_PATH);
 
-export const DB_PATH = `${__dirname}/../../../db/booCSS.db`;
+export const DB_PATH = path.join(__dirname,'..','..','..','db/booCSS.db');
